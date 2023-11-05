3a06f7c3c41636844e33a5c2a61cbed28b08c3ed
Stephan Lachnit <stephanlachnit@debian.org>
2023-11-05 14:16:05 +0100

    C++20 support
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- CHIRP/config.hpp
+++ CHIRP/config.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <version>
+
 #if (defined _WIN32 && !defined __CYGWIN__)
 #if CHIRP_BUILDLIB
 #define CHIRP_API __declspec(dllexport)
@@ -12,4 +14,30 @@
 #else
 #define CHIRP_API
 #endif
+#endif
+
+// std::to_underlying
+#ifndef __cpp_lib_to_underlying
+#include <type_traits>
+namespace std {
+template <typename E>
+constexpr typename std::underlying_type<E>::type to_underlying(E e) noexcept {
+    return static_cast<typename std::underlying_type<E>::type>(e);
+}
+}
+#endif
+
+// std::unreachable
+#ifndef __cpp_lib_unreachable
+namespace std{
+[[noreturn]] inline void unreachable() {
+    #ifdef __GNUC__
+    __builtin_unreachable();
+    #else
+    #ifdef _MSC_VER
+    __assume(false);
+    #endif
+    #endif
+}
+}
 #endif
